494a2ec4f12bc71fb312f8d048ab4394da328151
Landon Schlangen <landonschlangen@Landons-MacBook-Pro.local>
2020-08-27 16:32:50 -0500

    hamburger menu in progress

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,47 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/core";
 import GitHubIcon from "src/assets/githubIcon.png";
 import linkedinIcon from "src/assets/linkedinIcon.png";
+import menuIcon from "src/assets/menuIcon.png";
+import { NavbarLink } from "src/StyledComponents";
+import { Icon } from "@chakra-ui/core";
 
 function Navbar() {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  const [sidebar, setSidebar] = useState(false);
+
+  window.onresize = () => {
+    setWindowSize(window.innerWidth);
+  };
+
   return (
-    <Flex justifyContent="center" alignItems="center" py="10px">
-      <Box w="50%" d="flex" justifyContent="center">
-        <Link to="/">
+    <>
+      <Flex justifyContent="center" alignItems="center" py="10px" mx="10%">
+        <Box w="70%" d="flex">
+          <Link to="/">
+            <Box
+              color="Highlight"
+              fontSize="45px"
+              fontWeight="bold"
+              fontFamily="TimesNewRoman"
+            >
+              Landon Schlangen
+            </Box>
+          </Link>
+        </Box>
+        {windowSize > 860 && (
           <Box
-            color="Highlight"
-            fontSize="45px"
-            fontWeight="bold"
-            fontFamily="TimesNewRoman"
+            w="30%"
+            d="flex"
+            justifyContent="space-around"
+            alignItems="center"
           >
-            Landon Schlangen
+            <Link to="/">
+              <NavbarLink>Home</NavbarLink>
+            </Link>
+            <Link to="/projects">
+              <NavbarLink>Projects</NavbarLink>
+            </Link>
           </Box>
-        </Link>
-      </Box>
+        )}
 
-      <Box w="50%" d="flex" justifyContent="space-around" alignItems="center">
-        <Link to="/">
-          <Box color="ButtonText" fontSize="20px" fontWeight="bolder">
-            Home
+        {windowSize > 860 && (
+          <Box d="flex">
+            <Box marginRight="10px">
+              <a href="https://github.com/Landon345" target="_blank">
+                <img src={GitHubIcon} alt="Github" height="30px" />
+              </a>
+            </Box>
+            <Box>
+              <a href="https://www.linkedin.com/in/landon-schlangen-a3989a16b/">
+                <img src={linkedinIcon} alt="LinkedIn" height="30px" />
+              </a>
+            </Box>
           </Box>
-        </Link>
-        <Link to="/projects">
-          <Box color="ButtonText" fontSize="20px" fontWeight="bolder">
-            Projects
-          </Box>
-        </Link>
-      </Box>
+        )}
 
-      <Box d="flex" marginRight="40px">
-        <a href="https://github.com/Landon345" target="_blank">
-          <img src={GitHubIcon} alt="Github" height="30px" />
-        </a>
-        <a href="https://www.linkedin.com/in/landon-schlangen-a3989a16b/">
-          <img src={linkedinIcon} alt="LinkedIn" height="30px" />
-        </a>
-      </Box>
-    </Flex>
+        {windowSize <= 860 && (
+          <Box
+            onClick={() => setSidebar(!sidebar)}
+            position="absolute"
+            top="6"
+            right="40px"
+          >
+            {sidebar ? (
+              <Box>
+                <Icon name="close" size="30px" />
+              </Box>
+            ) : (
+              <Box>
+                <img src={menuIcon} alt="menu" height="30px" />
+              </Box>
+            )}
+          </Box>
+        )}
+      </Flex>
+    </>
   );
 }
 
---
+++ src/StyledComponents.tsx
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+//import PokeTheme from "../../PokeTheme";
+
+export const NavbarLink = styled.div`
+  text-decoration: none;
+  appearance: none;
+  background: transparent;
+  border: 0px;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 18px;
+  margin: 0px;
+  outline: none;
+  overflow: visible;
+  padding: 12px;
+  text-align: inherit;
+  text-decoration: none;
+  user-select: auto;
+  position: relative;
+  white-space: nowrap;
+  border-radius: 20px;
+  z-index: 1;
+  color: #222222;
+  &:hover {
+    background-color: #e9e9e9;
+  }
+`;
