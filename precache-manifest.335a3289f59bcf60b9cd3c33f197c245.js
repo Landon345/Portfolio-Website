bc288912a1e1a928b9f960c66cc04a79f51a6b65
Landon345 <landon.schlangen@ndsu.edu>
2020-09-06 16:59:37 -0500

    Updates

---
+++ precache-manifest.335a3289f59bcf60b9cd3c33f197c245.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea077b37b7c2f5d8909092790b2c9c6",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "6a1d5d7528ae7abab9d3",
+    "url": "/portfolio-website/static/css/main.279c5813.chunk.css"
+  },
+  {
+    "revision": "09d1a2baa2a77a6018eb",
+    "url": "/portfolio-website/static/js/2.da912e1c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/portfolio-website/static/js/2.da912e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1d5d7528ae7abab9d3",
+    "url": "/portfolio-website/static/js/main.18881a9a.chunk.js"
+  },
+  {
+    "revision": "99cb685b86efd0f580da",
+    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+  },
+  {
+    "revision": "e59b2463b5e6c85ec68c852d12f9079d",
+    "url": "/portfolio-website/static/media/AndrewKirby.e59b2463.jpg"
+  },
+  {
+    "revision": "a88387dc40395d01b8fdd27367e76b64",
+    "url": "/portfolio-website/static/media/BenAwad.a88387dc.jpg"
+  },
+  {
+    "revision": "508d3c4415f86a902e2f5461f856fd0b",
+    "url": "/portfolio-website/static/media/CleverProgrammer.508d3c44.jpg"
+  },
+  {
+    "revision": "c6a30140b33ad4a9b8dc32dd1c65f18f",
+    "url": "/portfolio-website/static/media/ContractingHome.c6a30140.jpg"
+  },
+  {
+    "revision": "9b6d14cd48e545ad6c926cb43cec682a",
+    "url": "/portfolio-website/static/media/ContractingLogin.9b6d14cd.jpg"
+  },
+  {
+    "revision": "67b9e33c3280b35e16298218d05f4012",
+    "url": "/portfolio-website/static/media/ContractingRegister.67b9e33c.jpg"
+  },
+  {
+    "revision": "e1880e886016f8a0eecedba94bb7ff34",
+    "url": "/portfolio-website/static/media/ContractingSearch1.e1880e88.jpg"
+  },
+  {
+    "revision": "47ea64752f05ed9a5bcc87f68aecc4fe",
+    "url": "/portfolio-website/static/media/ContractingSearch2.47ea6475.jpg"
+  },
+  {
+    "revision": "7415d319678ddcd70a2551498536151a",
+    "url": "/portfolio-website/static/media/CookieClickerCover.7415d319.png"
+  },
+  {
+    "revision": "d6690fd148da4894e7fbf3991f43159c",
+    "url": "/portfolio-website/static/media/FirstPokemon.d6690fd1.png"
+  },
+  {
+    "revision": "2f99e21e2854eee0756add6fb0501575",
+    "url": "/portfolio-website/static/media/FirstPokemonCover.2f99e21e.png"
+  },
+  {
+    "revision": "71c96c7d23f67994aa4c05ec21e556f8",
+    "url": "/portfolio-website/static/media/FirstPokemonDetail.71c96c7d.png"
+  },
+  {
+    "revision": "cb7af363d8cec844e9cdbecfc8af289e",
+    "url": "/portfolio-website/static/media/FrontendPokemon.cb7af363.png"
+  },
+  {
+    "revision": "cbd6824f163870f38d3bae9903f46843",
+    "url": "/portfolio-website/static/media/FrontendPokemonCover.cbd6824f.png"
+  },
+  {
+    "revision": "dc66f492a6fb0624693395d60d071589",
+    "url": "/portfolio-website/static/media/FrontendPokemonDetail.dc66f492.png"
+  },
+  {
+    "revision": "eb9054739c3570278b6900060ba61423",
+    "url": "/portfolio-website/static/media/FrontendPokemonSearch.eb905473.png"
+  },
+  {
+    "revision": "ea7a735edfd267dd97fbc0b5dac063fb",
+    "url": "/portfolio-website/static/media/FullStackPokemon.ea7a735e.png"
+  },
+  {
+    "revision": "a202bf7ed40a298794c2f986af5b17d4",
+    "url": "/portfolio-website/static/media/FullStackPokemonCaptured.a202bf7e.png"
+  },
+  {
+    "revision": "3ca03de42f98cd9c1aefd457d9d47650",
+    "url": "/portfolio-website/static/media/FullStackPokemonCover.3ca03de4.png"
+  },
+  {
+    "revision": "ba97ae4ec072ce53702cbc33ee8a5a90",
+    "url": "/portfolio-website/static/media/FullStackPokemonDetail.ba97ae4e.png"
+  },
+  {
+    "revision": "99ec23eaa7a1becb44807618c5769711",
+    "url": "/portfolio-website/static/media/GraphQLResumeCover.99ec23ea.jpg"
+  },
+  {
+    "revision": "1956df6991c2a1f6ab2172f6cbef792a",
+    "url": "/portfolio-website/static/media/GraphqlResume.1956df69.png"
+  },
+  {
+    "revision": "fa46a530f0bf4a65b441cb4d68920b8f",
+    "url": "/portfolio-website/static/media/HusseinNasser.fa46a530.jpg"
+  },
+  {
+    "revision": "7b6a8ecb0982878568e03f1e20d88caa",
+    "url": "/portfolio-website/static/media/LeighHalliday.7b6a8ecb.jpg"
+  },
+  {
+    "revision": "db2a6825f64472f77b1f748ededa6105",
+    "url": "/portfolio-website/static/media/TradingPractice.db2a6825.png"
+  },
+  {
+    "revision": "880a316cc871788e76891fff71e6f246",
+    "url": "/portfolio-website/static/media/TraversyMedia.880a316c.jpg"
+  },
+  {
+    "revision": "5d90c188a16a4a21c384331f908e8432",
+    "url": "/portfolio-website/static/media/WebDevSimplified.5d90c188.jpg"
+  },
+  {
+    "revision": "26fab60a1beabb0d2c21b1a96e7351ff",
+    "url": "/portfolio-website/static/media/WhatCanICookHome.26fab60a.jpg"
+  },
+  {
+    "revision": "f48310aae7b6163e335a85a927e79292",
+    "url": "/portfolio-website/static/media/WhatCanICookRecipes.f48310aa.jpg"
+  },
+  {
+    "revision": "4c953e19ab6f9cfc52ecf0585b2a2ca6",
+    "url": "/portfolio-website/static/media/amazon.4c953e19.png"
+  },
+  {
+    "revision": "e87d8b42c3bf9e1cc3baadd21a1593b9",
+    "url": "/portfolio-website/static/media/amazonCover.e87d8b42.png"
+  },
+  {
+    "revision": "b5d95ff15f2c5630b8f67c8784d1659d",
+    "url": "/portfolio-website/static/media/cookieclicker.b5d95ff1.png"
+  },
+  {
+    "revision": "1aebc5e699474440bfde2b473d2a1aa0",
+    "url": "/portfolio-website/static/media/covidTracker.1aebc5e6.png"
+  },
+  {
+    "revision": "4d860ed55d87c8442efcc07aaff6a9bd",
+    "url": "/portfolio-website/static/media/covidTrackerCover.4d860ed5.png"
+  },
+  {
+    "revision": "e36ee229ff7951b7bcd11e0d04aa65a8",
+    "url": "/portfolio-website/static/media/dealornodeal.e36ee229.png"
+  },
+  {
+    "revision": "cfe682069fcd7b3949e87685cc779277",
+    "url": "/portfolio-website/static/media/dealornodealCover.cfe68206.png"
+  },
+  {
+    "revision": "bdaa4a640ad875c696476cd2bcde9059",
+    "url": "/portfolio-website/static/media/homeRemodelContracting.bdaa4a64.png"
+  },
+  {
+    "revision": "5595f8744df121f0b153a03cc81e60c8",
+    "url": "/portfolio-website/static/media/instagram.5595f874.png"
+  },
+  {
+    "revision": "8bf8817aa7aa42167da63cbec357a189",
+    "url": "/portfolio-website/static/media/instagramCover.8bf8817a.png"
+  },
+  {
+    "revision": "af8747e80ac2b5791a3b45837f30812f",
+    "url": "/portfolio-website/static/media/netflix.af8747e8.png"
+  },
+  {
+    "revision": "05de3af80e8d754f432782fa40f5de13",
+    "url": "/portfolio-website/static/media/netflixCover.05de3af8.png"
+  },
+  {
+    "revision": "254d341684e6541bbb447bf9e70801b3",
+    "url": "/portfolio-website/static/media/tabsystem.254d3416.png"
+  },
+  {
+    "revision": "c8a9a90bfbbff8621d76af0a4f8b2a9e",
+    "url": "/portfolio-website/static/media/tabsystemCover.c8a9a90b.png"
+  },
+  {
+    "revision": "90d62a851bf5b4d8703e41fa2556fd21",
+    "url": "/portfolio-website/static/media/tabsystemMyTabs.90d62a85.png"
+  },
+  {
+    "revision": "7947463f179af17915d6e0b8cce32e6c",
+    "url": "/portfolio-website/static/media/tabsystemTabs.7947463f.png"
+  },
+  {
+    "revision": "11e5461ff4f3e70c54718ae062e288fd",
+    "url": "/portfolio-website/static/media/tinder.11e5461f.png"
+  },
+  {
+    "revision": "d0f46c30e15da71f3068957f5103638b",
+    "url": "/portfolio-website/static/media/tinderCover.d0f46c30.png"
+  },
+  {
+    "revision": "c6b932e6e28894ff91751f6d13fd602f",
+    "url": "/portfolio-website/static/media/whatCanICook.c6b932e6.png"
+  },
+  {
+    "revision": "4f5b94bd8c4c84e5d2274236da16b855",
+    "url": "/portfolio-website/static/media/youTube.4f5b94bd.png"
+  },
+  {
+    "revision": "e56ca871fe5ca5322c675983234024b9",
+    "url": "/portfolio-website/static/media/youTubeCover.e56ca871.png"
+  }
+]);
